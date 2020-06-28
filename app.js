5241b07d6c516a3221a4621378cae03055ff2099
entromin <1479894310@qq.com>
2020-06-28 14:29:46 +0800

    存证页面更新

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ try {
 }
 
 const app = express();
-app.set('port', process.env.PORT || 300);
+app.set('port', process.env.PORT || 3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
